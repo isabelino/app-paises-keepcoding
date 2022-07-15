a658d5b688bbe135a141b955a8d8c717b5d12144
rolando <isa.klopez@hotmail.com>
2022-07-15 14:48:41 +0200

    ultimos cambios

--- src/app/paises/pages/por-pais/por-pais.component.ts
+++ src/app/paises/pages/por-pais/por-pais.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent implements OnInit {
 
+  termino:string = "Hola mundo";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  buscar():void{
+    console.log(this.termino);
   }
 
 }
